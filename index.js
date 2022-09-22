e367da1f84c59258f437e038e7d77588956272e8
xuannbgch17346 <xuannbgch17346@fpt.edu.vn>
2022-09-22 05:26:43 -0700

    6ok

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const http = require("http");
 const port = process.env.PORT || 3000;
 
 const app = http.createServer((req, res) => {
-  res.write("<h1>xuan</h1>");
-  res.write("<h3> Xuan</h3>");
+  res.write("<h1> xuan </h1>");
+  res.write("<h3> Xuan </h3>");
   res.end();
 });
 
